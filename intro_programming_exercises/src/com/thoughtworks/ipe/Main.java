c55e535bcab8c5998602d6097f8b067e7e4a598d
Desiree Kelly <kelly.desiree@gmail.com>
2015-11-30 21:25:21 +1100

    Print one asterisk to the console

--- intro_programming_exercises/src/com/thoughtworks/ipe/Main.java
+++ intro_programming_exercises/src/com/thoughtworks/ipe/Main.java
@@ -1,9 +1,14 @@
 package com.thoughtworks.ipe;
 
+/**
+ * Created by Desiree Kelly on 30/11/15.
+ */
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        //Print one asterisk to the console
+        System.out.println("*");
     }
 }
