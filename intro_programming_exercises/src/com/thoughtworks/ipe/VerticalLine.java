4e9c0fdd8dfa3fbde976597d6bf0be4ede588e29
Desiree Kelly <kelly.desiree@gmail.com>
2015-11-30 21:59:59 +1100

    Print n asterisks on a vertical line

---
+++ intro_programming_exercises/src/com/thoughtworks/ipe/VerticalLine.java
@@ -0,0 +1,41 @@
+package com.thoughtworks.ipe;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+/**
+ * Given a number n, prints n lines, each with one asterisk.
+ *
+ * Created by Desiree Kelly on 30/11/2015.
+ */
+public class VerticalLine {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int number = -1;
+
+        while(number < 0){
+
+            try{
+
+                System.out.print("Enter a positive integer: ");
+
+                //get number input from console
+                number = input.nextInt();
+            }
+            catch(InputMismatchException e){
+
+                //display error if number is not a positive integer
+                System.out.println("Invalid Format");
+                input.nextLine();
+            }
+        }
+
+        //print n number of asterisks on a vertical line based on console input
+        for(int x = 0; x < number; x++){
+            System.out.println("*");
+        }
+    }
+}
