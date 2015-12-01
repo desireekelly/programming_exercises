16689a5a7f6bbe4ac6a6d4889a9603f6a0998488
Desiree Kelly <kelly.desiree@gmail.com>
2015-12-01 16:49:13 +1100

    Prime Factors

--- intro_programming_exercises/src/com/desireekelly/ipe/PrimeFactors/PrimeFactors.java
+++ intro_programming_exercises/src/com/desireekelly/ipe/PrimeFactors/PrimeFactors.java
@@ -1,14 +1,66 @@
 package com.desireekelly.ipe.PrimeFactors;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 /**
- * Created by Desiree on 30/11/2015.
+ * Given an integer n will return a list of integers such that the numbers are the factors of n and are arranged in
+ * increasing numerical order.
+ *
+ * Created by Desiree Kelly on 1/12/2015.
  */
 public class PrimeFactors {
 
     public static void main (String[] args){
 
+        Scanner input = new Scanner(System.in);
 
+        int number = -1;
 
+        while(number < 0){
 
+            try{
+
+                System.out.print("Enter a positive integer: ");
+
+                //get number input from console
+                number = input.nextInt();
+            }
+            catch(InputMismatchException e){
+
+                //display error if number is not a positive integer
+                System.out.println("Invalid Format");
+                input.nextLine();
+            }
+        }
+
+        System.out.println(generate(number));
+    }
+
+    public static ArrayList<Integer> generate(int n){
+
+        //ArrayList to hold factors
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        //loop through each factor x
+        for(int x = 2; x * x <= n; x++){
+
+            //if x is a factor of n divide
+            while(n % x == 0){
+                numbers.add(x);
+                n = n / x;
+            }
+        }
+
+        if(n > 1){
+            numbers.add(n);
+        }
+
+        //Sort ArrayList in ascending order
+        Collections.sort(numbers);
+
+        return numbers;
     }
 }
