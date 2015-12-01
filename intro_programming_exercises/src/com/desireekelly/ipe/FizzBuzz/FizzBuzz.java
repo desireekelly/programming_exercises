d88a007e431f73ae9af89c2330e3bb109c1a8952
Desiree Kelly <kelly.desiree@gmail.com>
2015-12-01 15:39:56 +1100

    FizzBuzz method

--- intro_programming_exercises/src/com/desireekelly/ipe/FizzBuzz/FizzBuzz.java
+++ intro_programming_exercises/src/com/desireekelly/ipe/FizzBuzz/FizzBuzz.java
@@ -4,4 +4,28 @@ package com.desireekelly.ipe.FizzBuzz;
  * Created by Desiree on 30/11/2015.
  */
 public class FizzBuzz {
+
+    public static void main(String[] args) {
+        FizzBuzz();
+
+    }
+
+    public static void FizzBuzz() {
+
+        for (int x = 1; x <= 100; x++) {
+
+            if ((x % 3 == 0) && (x % 5 != 0)) {
+                System.out.println("Fizz");
+            }
+            else if ((x % 5 == 0) && (x % 3 != 0)) {
+                System.out.println("Buzz");
+            }
+            else if ((x % 3 == 0) && (x % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            }
+            else {
+                System.out.println(x);
+            }
+        }
+    }
 }
